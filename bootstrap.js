b00f361efe5b41d838b16993de80bac3ff65af3a
r3dh4r7 <redhart@dualsight.org>
2019-10-09 12:17:30 +0100

    Refactor.

--- bootstrap.js
+++ bootstrap.js
@@ -6,7 +6,7 @@ const configInit = require('./lib/config/init')
 if (fs.existsSync(path.join(__dirname, 'config.json'))) {
   screen.error('Failed to bootstrap corepay: Configuration file: config.json already exists!')
 } else {
-  fs.writeFileSync('config.json', JSON.stringify(configInit.generate(), null, 2), { encoding: 'UTF-8' })
+  fs.writeFileSync(path.join(__dirname, 'config.json'), JSON.stringify(configInit.generate(), null, 2), { encoding: 'UTF-8' })
   screen.success('Configuration file generated: config.json')
   screen.info('Please inspect the file to obtain auto-generated passphrases and mnemonics.')
 }
